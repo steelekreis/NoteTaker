aff335764b07158feccbc6881c6c62f4468ec220
steelekreis <steele.kreis@gmail.com>
2022-02-01 21:10:53 -0700

    switched around routes as notes needs to be first apparently

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const router = require('express').Router();
 
-//catchall route that sends user to index
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 //notes page route
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
+
+
+//catchall route that sends user to index
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 
 module.exports = router;
